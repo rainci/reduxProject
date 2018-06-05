2a494254eb9de7d8f22e99cbd151bc258fc44afc
liuyuxi520 <2805242676@qq.com>
2018-06-05 21:58:25 +0800

    add body com

---
+++ redux-seven/src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import { Layout, Menu, Breadcrumb, Icon, Button } from 'antd';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+
+const Body = () => {
+    return (
+        <Layout>
+          <h1>lyx的body</h1>
+          {/* {this.props.children} */}
+        </Layout>
+    )
+}
+export default Body;
