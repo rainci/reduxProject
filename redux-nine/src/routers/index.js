60ba595b4dbdc69d79382857e17d34ec39b07f99
liuyuxi520 <2805242676@qq.com>
2018-06-20 18:45:15 +0800

    add whether login

--- redux-nine/src/routers/index.js
+++ redux-nine/src/routers/index.js
@@ -1,88 +1,111 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Tbd from './Tbd'
-import Home from './Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
 
-const App = () => {
-    return (
-        <div>
-            我是公共的
-            <Route path="/home" component={Home} />
-            <Route path="/tbd" component={Tbd} />
-        </div>
-    )
-}
-const Foot = () => {
-    return (
-        <ul>
-            <li>
-                <Link to="/home">About Us1 (static)</Link>
-            </li>
-            <li>
-                <Link to="/tbd">Company (static)</Link>
-            </li>
-        </ul>
-    )
-}
-const Routers = () => (
+const LogRouter = () => (
+  <Router>
     <div>
-        <BrowserRouter>
-            <div>
-                <Foot />
-                <Route path="/" component={App} />
-            </div>
-        </BrowserRouter>
+      <AuthBtn />
+      <ul>
+        <li>
+          <Link to='/public'>Public page</Link>
+        </li>
+        <li>
+          <Link to='/protect'>Protected page</Link>
+        </li>
+      </ul>
+      <Route path='/public' component={Public} />
+      <PriviteRoute path='/protect' component={Protect} />
+      <Route path='/login' component={Login} />
+
     </div>
+  </Router>
+);
+const pageStore = {
+  islogin: false,
+  login(cb) {
+    this.islogin = true;
+    setTimeout(cb,100);
+  },
+  logout(cb) {
+    this.islogin = false;
+    setTimeout(cb,100);
+  }
+};
+const Public = () => (<h2>public page</h2>);
+const Protect = () => (<h2>protect page</h2>);
+
+const AuthBtn = withRouter(
+  ({ history }) => pageStore.islogin ? (
+    <p>
+      Welcome!
+      <button onClick={() => {
+        pageStore.logout(() => history.push('/'))
+      }}>sign out!</button>
+    </p>
+  ) : (
+      <p>You are not logged in.</p>
+    )
+);
+
+class Login extends React.Component {
+  constructor(prop){
+    super(prop);
+    this.login = this.login.bind(this);
+    this.state = {
+      refer: false
+    }
+  }
+  login(){
+    pageStore.login(() => {
+      this.setState({refer: true});
+    })
+  }
+  render() {
+      const { from } = this.props.location.state || { from: { pathname: "/" } };
+      const { refer } = this.state;
+      if(refer){
+        return <Redirect to={from} />;
+      }
+      return(
+        <div>
+          <p>You must log in to view the page at {from.pathname}</p>
+          <p>
+            <button onClick={this.login}>login!</button>
+          </p>
+        </div>
+      )
+  }
+}
+
+const PriviteRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      pageStore.islogin ? (
+        <Component {...props} />
+      ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+    )
+
+    }
+  />
 );
 
 
-const ParamsExample = () => (
-    <BrowserRouter>
-      <div>
-        <h2>Accounts</h2>
-        <ul>
-          <li>
-            <Link to="/netflix">Netflix</Link>
-          </li>
-          <li>
-            <Link to="/zillow-group">Zillow Group</Link>
-          </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/modus-create">Modus Create</Link>
-          </li>
-        </ul>
-  
-        <Route path="/:id" component={Child} />
-  
-        {/*
-           It's possible to use regular expressions to control what param values should be matched.
-              * "/order/asc"  - matched
-              * "/order/desc" - matched
-              * "/order/foo"  - not matched
-        */}
-        <Route
-          path="/order/:direction(asc|desc)"
-          component={ComponentWithRegex}
-        />
-      </div>
-    </BrowserRouter>
-  );
-  
-  const Child = ({ match }) => (
-    <div>
-      <h3>ID: {match.params.id}</h3>
-    </div>
-  );
-  
-  const ComponentWithRegex = ({ match }) => (
-    <div>
-      <h3>Only asc/desc are allowed: {match.params.direction}</h3>
-    </div>
-  );
-  
-// export default Routers;
-export default ParamsExample;
-// export default {Routers,ParamsExample};
+
+
+
+export default LogRouter;
+
