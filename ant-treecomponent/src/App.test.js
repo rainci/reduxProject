5a9a804b32f304411d309fab5aa00db871032ff2
liuyuxi520 <2805242676@qq.com>
2018-12-09 16:01:18 +0800

    add test

--- ant-treecomponent/src/App.test.js
+++ ant-treecomponent/src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
+function setup() {
+  const enzymeWrapper = mount( <App /> )
+​
+  return {
+    enzymeWrapper
+  }
+}
+
+
+describe('components', () => {
+  describe('App', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setup();
+​
+      expect(enzymeWrapper.find('div').hasClass('App')).toBe(true)
+​
+      const TaskTagTreeList = enzymeWrapper.find('TaskTagTreeList').props()
+      expect(TaskTagTreeList.treeData).toBe(Array)
+    })
+​
+  })
+})
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
---
+++ ant-treecomponent/src/components/tree/userSearch.test1.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {Array} searchNames 
+ * @param {Function} onSearchFn  
+ * @return {component} UserSearch 
+ * @author rainci(刘雨熙)
+ */
+import React from 'react';
+import UserSearch from './UserSearch';
+
