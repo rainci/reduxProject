f924df5e841ca35db52de57300eb72e31d11da6c
univer <univer@univerdeiMac-2.local>
2019-05-16 19:17:03 +0800

    add

--- react-swipe/src/pages/swiper.js
+++ react-swipe/src/pages/swiper.js
@@ -10,9 +10,15 @@ class New extends Component {
         // }
     }
     componentDidMount() {
-        new Swiper('.swiper-container', {
+        var mySwiper = new Swiper('.swiper-container', {
             slidesPerView: 1,
-            centeredSlides: true,
+            // centeredSlides: true,
+            loop: true,
+            autoplay:3000,
+            // autoplay: {
+            //     delay:2000,
+            //     disableOnInteraction: false
+            // },
             // virtual: {
             //     slides: this.state.newlist,
             // },
@@ -22,19 +28,38 @@ class New extends Component {
                 prevEl: '.swiper-button-prev',
             },
         })
+        // mySwiper.el.mouseover = function(){ //鼠标放上暂停轮播
+        //     mySwiper.autoplay.stop();
+        // }
+        // mySwiper.el.mouseleave = function(){
+        //     mySwiper.autoplay.start();
+        // }
+        // var slide = document.getElementsByClassName('swiper-slide');
+        // for(var i=0; i< slide.length; i++){
+        //     slide[i].onmouseenter = function(){
+        //         mySwiper.autoplay.stop();
+        //     }
+        // }
+        // console.log(slide)
+        // document.getElementsByClassName('swiper-slide').mouseenter(function () {
+            // mySwiper.autoplay.stop();
+        //  })
+        //  document.getElementsByClassName('swiper-slide')[0].mouseleave(function () {
+        //     mySwiper.autoplay.start();
+        //  })
     }
     render() {
         return (
             <div className='new' style={{ 'width': '300px', 'height': '300px', 'background': 'pink' }}>
                 <div className="swiper-container" style={{ 'width': '300px', 'height': '300px', 'background': 'pink' }}>
                     <div className="swiper-wrapper" >
-                        <div class="swiper-slide">Slide 1</div>
-                        <div class="swiper-slide">Slide 2</div>
-                        <div class="swiper-slide">Slide 3</div>
+                        <div className="swiper-slide">Slide 1</div>
+                        <div className="swiper-slide">Slide 2</div>
+                        <div className="swiper-slide">Slide 3</div>
                     </div>
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
+                    {/* <div className="swiper-pagination"></div> */}
+                    <div className="swiper-button-prev"></div>
+                    <div className="swiper-button-next"></div>
                 </div>
             </div>
         )
