f7150c77aebfea55c5774c3686cee4bf923aa30e
univer <univer@univerdeiMac-2.local>
2019-07-17 15:17:49 +0800

    add tree

--- ant-treecomponent/src/pages/tableList.js
+++ ant-treecomponent/src/pages/tableList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import server from '../api/server';
-import {message} from 'antd';
+import {message, Table} from 'antd';
 import TableListCheck from '../components/table/tableListCheck';
 const columnsTable = [
   {
@@ -26,7 +26,86 @@ const columnsTable = [
     }
   },
 ];
-
+const columnsTable2 = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="javascript:;">{text}</a>,
+  }, {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  }, {
+    title: '地址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+const data = [
+  {
+    key: 1,
+    name: 'John Brown sr.',
+    age: 60,
+    address: 'New York No. 1 Lake Park',
+    children: [
+      {
+        key: 11,
+        name: 'John Brown',
+        age: 42,
+        address: 'New York No. 2 Lake Park',
+      },
+      {
+        key: 12,
+        name: 'John Brown jr.',
+        age: 30,
+        address: 'New York No. 3 Lake Park',
+        children: [
+          {
+            key: 121,
+            name: 'Jimmy Brown',
+            age: 16,
+            address: 'New York No. 3 Lake Park',
+          },
+        ],
+      },
+      {
+        key: 13,
+        name: 'Jim Green sr.',
+        age: 72,
+        address: 'London No. 1 Lake Park',
+        children: [
+          {
+            key: 131,
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 2 Lake Park',
+            children: [
+              {
+                key: 1311,
+                name: 'Jim Green jr.',
+                age: 25,
+                address: 'London No. 3 Lake Park',
+              },
+              {
+                key: 1312,
+                name: 'Jimmy Green sr.',
+                age: 18,
+                address: 'London No. 4 Lake Park',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 2,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
 class TableList extends Component {
   constructor(props) {
     super(props)
@@ -76,6 +155,11 @@ class TableList extends Component {
           tableListData={this.state.tableListData}
           selectedRowKeys={this.state.selectedRowKeys}
           onTableListCheck={this.tableListCheckFn}
+        />
+        <Table
+          columns={columnsTable2}
+          dataSource={data} 
+          rowKey={record => record.key}
         />
       </div>
     );
