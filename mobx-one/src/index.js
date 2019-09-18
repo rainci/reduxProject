c62705b9872aeef7911828f84aa901f21934c28a
rainci <2805242676@qq.com>
2019-09-18 16:02:36 +0800

    add mobx test

--- mobx-one/src/index.js
+++ mobx-one/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { observable, action } from "mobx";
+import { observer } from "mobx-react";
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create State object
+let appState = observable({ timer: 1, title:'test mb' });
+appState.resetTimer = action(() => {
+    appState.timer = 0;
+});
+// define action
+setInterval(
+    action(() => {
+        appState.timer += 1;
+    }),
+    1000
+);
+let App = observer(({ appState }) => {
+    return (
+        <div className="App">
+            <h1>Time passed: {appState.timer}</h1>
+            <button onClick={appState.resetTimer}>reset timer</button>
+        </div>
+    );
+});
+
+ReactDOM.render(<App appState={appState} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
